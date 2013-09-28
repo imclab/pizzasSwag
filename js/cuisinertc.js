dff525be518d90b85e642731243f54ab3fdea105
Romain Bertrand <robertrand.ext@orange.com>
2013-09-28 15:18:49 +0100

    add

---
+++ js/cuisinertc.js
@@ -0,0 +1,127 @@
+//$(document).ready(function(){
+
+
+    var config = {
+        openSocket: function (config) {
+            var SIGNALING_SERVER = 'https://www.webrtc-experiment.com:8553/',
+            defaultChannel = location.hash.substr(1) || 'webrtc-oneway-broadcasting';
+
+            var channel = config.channel || defaultChannel;
+            var sender = Math.round(Math.random() * 999999999) + 999999999;
+
+            io.connect(SIGNALING_SERVER).emit('new-channel', {
+                channel: channel,
+                sender: sender
+            });
+
+            var socket = io.connect(SIGNALING_SERVER + channel);
+            socket.channel = channel;
+            socket.on('connect', function() {
+                if (config.callback) config.callback(socket);
+            });
+
+            socket.send = function(message) {
+                socket.emit('message', {
+                    sender: sender,
+                    data: message
+                });
+            };
+
+            socket.on('message', config.onmessage);
+        },
+        onRemoteStream: function (htmlElement) {
+            //videosContainer.insertBefore(htmlElement, videosContainer.firstChild);
+            //htmlElement.play();
+            //rotateInCircle(htmlElement);
+        },
+        onRoomFound: function (room) {
+            var alreadyExist = document.querySelector('button[data-broadcaster="' + room.broadcaster + '"]');
+            if (alreadyExist) return;
+
+            if (typeof roomsList === 'undefined') roomsList = document.body;
+
+            var broadcaster = room.broadcaster;
+            var roomToken = room.broadcaster;
+            broadcastUI.joinRoom({
+                roomToken: roomToken,
+                joinUser: broadcaster
+            });
+        },
+        onNewParticipant: function (numberOfViewers) {
+            document.title = 'Viewers: ' + numberOfViewers;
+        }
+    };
+
+    function setupNewBroadcastButtonClickHandler() {
+        document.getElementById('setup-new-broadcast').disabled =true;
+        $('#setup-new-broadcast').fadeOut();
+
+        captureUserMedia(function () {
+            var shared = 'video';
+            broadcastUI.createRoom({
+                roomName: 'pizzaswagboulzor',
+                isAudio: shared === 'audio'
+            });
+        });
+        hideUnnecessaryStuff();
+    }
+
+    function captureUserMedia(callback) {
+        var constraints = {
+            audio: false,
+            video: true
+        };
+
+        var htmlElement = document.createElement('video');
+        htmlElement.setAttribute('autoplay', true);
+        videosContainer.insertBefore(htmlElement, videosContainer.firstChild);
+
+        var mediaConfig = {
+            video: htmlElement,
+            onsuccess: function (stream) {
+                config.attachStream = stream;
+                callback && callback();
+
+                htmlElement.setAttribute('muted', true);
+                rotateInCircle(htmlElement);
+            },
+            onerror: function () {
+                if (option === 'Only Audio') alert('unable to get access to your microphone');
+                else if(option === 'Screen') {
+                    if(location.protocol === 'http:') alert('Please test this WebRTC experiment on HTTPS.');
+                    else alert('Screen capturing is either denied or not supported. Are you enabled flag: "Enable screen capture support in getUserMedia"?');
+                }
+                else alert('unable to get access to your webcam');
+            }
+        };
+        if (constraints) mediaConfig.constraints = constraints;
+        getUserMedia(mediaConfig);
+    }
+
+    var broadcastUI = broadcast(config);
+
+    /* UI specific */
+    var videosContainer = document.getElementById('videos-container') || document.body;
+    var setupNewBroadcast = document.getElementById('setup-new-broadcast');
+    var roomsList = document.getElementById('rooms-list');
+
+    var broadcastingOption = document.getElementById('broadcasting-option');
+
+    if (setupNewBroadcast) setupNewBroadcast.onclick = setupNewBroadcastButtonClickHandler;
+
+    function hideUnnecessaryStuff() {
+        var visibleElements = document.getElementsByClassName('visible'),
+            length = visibleElements.length;
+        for (var i = 0; i < length; i++) {
+            visibleElements[i].style.display = 'none';
+        }
+    }
+
+    function rotateInCircle(video) {
+        video.style[navigator.mozGetUserMedia ? 'transform' : '-webkit-transform'] = 'rotate(0deg)';
+        setTimeout(function () {
+            video.style[navigator.mozGetUserMedia ? 'transform' : '-webkit-transform'] = 'rotate(360deg)';
+        }, 1000);
+    }
+
+//});
--- js/script.js
+++ js/script.js
@@ -6,47 +6,6 @@ $(document).ready(function(){
 			goToVroumVroum();
 		}
 	});
-
-
-        // GET THE SWAAAAAAGY VIDEO FEEEEEEEEEEEEED!!!!!!!!!!
-        var peer = new PeerConnection('ws://192.168.1.130:1338');
-                peer.onUserFound = function(userid) {
-                    if (document.getElementById(userid)) return;
-                    peer.sendParticipationRequest(userid);
-                };
-
-                peer.onStreamAdded = function(e) {
-                    console.log(document.getElementById('kitchenWindow'))
-                    if (e.type == 'local') return;
-                    var video = e.mediaElement;
-                    //var video = document.getElementById('kitchen');
-
-                    video.setAttribute('width', 600);
-                    video.setAttribute('controls', false);
-
-                    var videosContainer = document.getElementById('kitchenWindow');
-                    videosContainer.insertBefore(video, document.getElementById('kitchen'));
-
-                    video.play();
-                };
-
-                peer.onStreamEnded = function(e) {
-                    var video = e.mediaElement;
-                    if (video) {
-                        video.style.opacity = 0;
-                        rotateVideo(video);
-                        setTimeout(function() {
-                            video.parentNode.removeChild(video);
-                        }, 1000);
-                    }
-                };
-
-                var videosContainer = document.getElementById('videos-container') || document.body;
-                var btnSetupNewRoom = document.getElementById('setup-new-room');
-                var roomsList = document.getElementById('rooms-list');
-
-
-
 });
 
 function goToKitchenCam() {
